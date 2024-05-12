deb2781e77a59d6515952d76d86d988a2c9a56ea
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-05-12 12:32:58 -0400

    Update index.js

--- docs/20240508/index.js
+++ docs/20240508/index.js
@@ -22,6 +22,10 @@ function start([ Interface ]) {
   try {
     document.body.style.fontSize = "48pt";
     document.body.append("Click to Start");
+    const masterInterface = document.createElement("iframe");
+    masterInterface.style.display = "none";
+    masterInterface.src = "https://scotwatson.github.io/WebInterface/20240316/index.html";
+    document.body.append(masterInterface);
     document.body.addEventListener("click", function () {
       (async function () {
         const file = await Interface.modalSingleFile({
@@ -34,7 +38,7 @@ function start([ Interface ]) {
         }
         switch (fileDocument.documentElement.getAttribute("version") === "0") {
           case "0": {
-            
+            openVersion0(fileDocument)
           }
             break;
           default: {
@@ -42,21 +46,40 @@ function start([ Interface ]) {
           }
             break;
         }
-        function version0() {
+        function openVersion0(fileDocument) {
           const resources = [];
           for (const resourceNode of fileDocument.documentElement.children) {
             if (resourceNode.nodeName !== "resource") {
               throw "Invalid node";
             }
             const resource = {
-              icon: resourceNode.getAttribute("icon"),
-              url: resourceNode.getAttribute("url"),
               description: resourceNode.innerHTML,
             };
+            for (const attribute of resourceNode.attributes) {
+              resource[attribute.name] = attribute.value;
+            }
             resources.push(resource);
           }
+          return {
+            resources,
+          };
+        }
+        function save(fileObject) {
+          const fileDocument = document.implementation.createDocument(null, "resources");
+          fileDocument.documentElement.setAttribute("version", "0");
+          for (const resource of fileObject.resources) {
+            const resourceNode = fileDocument.createElement("resource");
+            for (const property of Object.getOwnProperyNames(resource)) {
+              resourceNode.setAttribute(property, resource[property]);
+            }
+            fileDocument.documentElement.append(resourceNode);
+          }
+          return fileDocument;
         }
         function createEditor() {
+          
+        }
+        function createViewer() {
           
         }
       })();
